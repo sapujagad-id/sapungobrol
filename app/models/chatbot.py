f770b6d34237cb88cdc66f89fbaa1ea58b1b8ac5
Veivel <25278964+Veivel@users.noreply.github.com>
2024-09-21 18:58:52 +0700

    reorganize commits: revert "feat: implement chatbot model and API"
    This reverts commit 25758505068289b974b3720ec7aebe215c3c9686.

--- app/models/chatbot.py
+++ app/models/chatbot.py
@@ -1,39 +1,9 @@
-from datetime import datetime
-import enum
-from sqlalchemy import Column, Enum, Uuid, String, Integer, Boolean, Text, DateTime
+from enum import Enum
+from sqlalchemy import Column, Uuid, String, Integer, Boolean, Text
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
   
-class ModelEngine(enum.Enum):
-  OPENAI = "OpenAI"
-  ANTHROPIC = "Anthropic"
   
-class MessageAdapter(enum.Enum):
-  SLACK = "Slack"
-  
-class DataType(enum.Enum):
-  SQL = "SQL"
-  
-class Chatbot(Base):
-  '''
-  A chatbot instance.
-  '''
-  __tablename__ = "chatbot"
-  
-  id = Column(Uuid, primary_key=True)
-  name = Column(String(255), nullable=False)
-  system_prompt = Column(Text(length=2048), nullable=False)
-  model = Column(Enum(ModelEngine), nullable=False)
-  data_source = Column(Enum(DataType), nullable=False)
-  created_at = Column(DateTime, default=datetime.now)
-  updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-  
-  # required fields for SQL Data Type
-  url = Column(String(127))
-  tables = Column(String(255))
-  
-  def __repr__(self):
-    return f"Chatbot id:{self.id}"
   
   
