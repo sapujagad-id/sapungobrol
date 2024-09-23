55257b31529255cdbdc8e64df471aa6d726d098a
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2024-09-23 17:07:49 +0700

    [GREEN] chore: update app config to include log level

--- config/__init__.py
+++ config/__init__.py
@@ -6,9 +6,15 @@ class AppConfig:
     def __init__(self) -> None:
         missing_values = False
 
+        log_level = os.getenv("LOG_LEVEL", "")
+        if log_level == "":
+            logging.error("config error: missing 'LOG_LEVEL'")
+            missing_values = True
+        self.log_level = log_level
+
         database_url = os.getenv("DATABASE_URL", "")
         if database_url == "":
-            logging.error("config error: missing 'database_url'")
+            logging.error("config error: missing 'DATABASE_URL'")
             missing_values = True
         self.database_url = database_url
 
