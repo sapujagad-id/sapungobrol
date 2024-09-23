21f57ba868f5e8670c4780d5efc8e6f8958262c4
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2024-09-23 16:58:41 +0700

    [RED] test: add case for log level

--- config/test_config.py
+++ config/test_config.py
@@ -5,4 +5,5 @@ class TestAppConfig:
     def test_config(self):
         config = AppConfig()
 
-        assert config.database_url == "postgresql://postgres:postgres@localhost:5432"
+        assert config.log_level == "info"
+        assert config.database_url == "postgresql://postgres:postgres@localhost:5433"
