e1c67cf9307e1ec5d091992e62f7dc6f44f59b61
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2024-09-24 18:20:26 +0700

    refactor: use explicit dependency injection

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 from dotenv import load_dotenv
-from fastapi import FastAPI, Depends
-from pydantic import BaseModel
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
-from app.api.routes import api_router
-from db import Group, get_db_session, get_group
+from fastapi import FastAPI, status
 
-app = FastAPI()
+from config import AppConfig, configure_logger
+from db import config_db
+from bot import Bot, BotControllerV1, BotServiceV1, PostgresBotRepository
 
-@app.get("/", response_model=Group | None)
-def index(db: Session = Depends(get_db_session)):
-    return get_group(db)
+import uvicorn
 
-app.include_router(api_router)
+load_dotenv(override=True)
+
+
+if __name__ == "__main__":
+    config = AppConfig()
+
+    configure_logger(config.log_level)
+
+    sessionmaker = config_db(config.database_url)
+
+    bot_repository = PostgresBotRepository(sessionmaker)
+
+    bot_service = BotServiceV1(bot_repository)
+
+    bot_controller = BotControllerV1(bot_service)
+
+    app = FastAPI()
+
+    app.add_api_route(
+        "/bots",
+        endpoint=bot_controller.fetch_chatbots,
+        status_code=status.HTTP_200_OK,
+        response_model=list[Bot],
+    )
+    app.add_api_route(
+        "/bots",
+        endpoint=bot_controller.create_chatbot,
+        methods=["POST"],
+        status_code=status.HTTP_201_CREATED,
+    )
+
+    uvicorn.run(app, host="0.0.0.0", port=config.port)
