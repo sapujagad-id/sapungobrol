b3db703928bcc67b229e5ec577fb56770e979ccf
Veivel <25278964+Veivel@users.noreply.github.com>
2024-09-25 20:55:44 +0700

    green: tests for bot module

--- bot/test_bot.py
+++ bot/test_bot.py
@@ -1,25 +1,49 @@
-from .bot import BotCreate, NameIsRequired, SystemPromptIsRequired, UnsupportedModel
+from .bot import BotCreate, NameIsRequired, SystemPromptIsRequired, UnsupportedAdapter, UnsupportedModel
 
 import pytest
 
 
 class TestBotCreate:
     def test_empty_name(self):
-        request = BotCreate(name="", system_prompt="Non-empty prompt", model="OpenAI")
+        request = BotCreate(
+            name="",
+            system_prompt="Non-empty prompt",
+            model="OpenAI",
+            adapter="Slack"
+        )
         with pytest.raises(NameIsRequired):
             request.validate()
 
     def test_empty_system_prompt(self):
-        request = BotCreate(name="Customer Report", system_prompt="", model="OpenAI")
+        request = BotCreate(
+            name="Customer Report", 
+            system_prompt="",
+            model="OpenAI",
+            adapter="Slack"
+        )
         with pytest.raises(SystemPromptIsRequired):
             request.validate()
-
+            
     def test_unsupported_model(self):
         request = BotCreate(
-            name="Customer Report", system_prompt="Non-empty prompt", model="ClosedAI"
+            name="Customer Report", 
+            system_prompt="Non-empty prompt", 
+            adapter="Slack",
+            model="ClosedAI"
         )
 
         with pytest.raises(UnsupportedModel):
+            request.validate()
+    
+    def test_unsupported_adapter(self):
+        request = BotCreate(
+            name="Customer Report", 
+            system_prompt="Non-empty prompt", 
+            adapter="WeChatV2",
+            model="OpenAI"
+        )
+
+        with pytest.raises(UnsupportedAdapter):
             request.validate()
 
     def test_create_bot_ok(self):
@@ -27,6 +51,7 @@ class TestBotCreate:
             {
                 "name": "Customer Service",
                 "system_prompt": "Non-empty prompt",
+                "adapter": "Slack",
                 "model": "OpenAI",
             },
         ]
