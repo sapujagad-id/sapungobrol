37ea87de4bcca29291147ee5fcbdfca09fd1f058
nyoosteven <stevenchrist123@gmail.com>
2024-09-28 23:33:13 +0700

    feat: add config openai_key_api

--- config/__init__.py
+++ config/__init__.py
@@ -49,6 +49,10 @@ class AppConfig:
         if not found:
             invalid = True
 
+        self.openai_key_api, found = self.validate_env_var("OPENAI_API_KEY")
+        if not found:
+            invalid = True
+            
         if invalid:
             raise ValueError("invalid app config")
 
