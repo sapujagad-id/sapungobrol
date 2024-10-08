ff6876daf03a3986774c5a59cb9d1545991e49c5
arkan.alexei <arkan.alexei@ui.ac.id>
2024-10-09 00:00:54 +0700

    [GREEN] refactor: test_selector so uses anthropic api key

--- chat/test_selector.py
+++ chat/test_selector.py
@@ -8,11 +8,11 @@ from .openai_chat import ChatOpenAI
 
 class TestChatEngineSelector:
     @pytest.fixture(scope="session")
-    def openai_api_key(self) -> str:
+    def api_key(self) -> str:
         return "Random-str"
 
-    def test_select_openai_engine(self, openai_api_key):
-        engine_selector = ChatEngineSelector(openai_api_key=openai_api_key)
+    def test_select_openai_engine(self, api_key):
+        engine_selector = ChatEngineSelector(openai_api_key=api_key, anthropic_api_key=api_key)
         engine = engine_selector.select_engine(ModelEngine.OPENAI)
 
         assert isinstance(engine, ChatOpenAI)
