f53fa1b221a6776a78d3480c6715da353f756010
Veivel <25278964+Veivel@users.noreply.github.com>
2024-10-10 22:06:08 +0700

    feat: auth DTOs

---
+++ auth/dto.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+
+from auth.repository import UserModel
+from auth.user import User
+
+class LoginResponse(BaseModel):
+  access_token: str
+
+class ProfileResponse(BaseModel):
+  data: User
+
+class GoogleCredentials(BaseModel):
+  client_id: str
+  client_secret: str
+  redirect_uri: str
+  
+  
+  
+class NoTokenSupplied(Exception):
+  pass
+
+class InvalidToken(Exception):
+  pass
+
+class UserNotFound(Exception):
+  pass
