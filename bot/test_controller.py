ae98684b3eb237a44f77fea9c1dfd91a21290525
jasonirvine76 <jason.irvine@ui.ac.id>
2024-10-18 20:36:37 +0700

    [RED] test: add unit test for checking slug

--- bot/test_controller.py
+++ bot/test_controller.py
@@ -70,6 +70,30 @@ class TestBotControllerCreate:
         assert bot[0].system_prompt == "some system prompt"
         assert bot[0].slug == "bot-a"  # Check slug
 
+    def test_create_chatbot_slug_exist(self, setup_controller):
+        create_request = BotCreate(
+            name="Bot A",
+            system_prompt="some system prompt",
+            model="OpenAI",
+            adapter="Slack",
+            slug="bot-a"  
+        )
+
+        create_request_1 = BotCreate(
+            name="Bot AB",
+            system_prompt="some system prompt",
+            model="OpenAI",
+            adapter="Slack",
+            slug="bot-a"  
+        )
+        
+        setup_controller.create_chatbot(create_request)
+        with pytest.raises(HTTPException) as exc:
+            setup_controller.create_chatbot(create_request_1)
+        
+        assert exc.value.status_code == 400
+        assert exc.value.detail == "Slug is exist"
+
     def test_create_chatbot_name_required(self, setup_controller):
         create_request = BotCreate(
             name="",
@@ -190,6 +214,44 @@ class TestBotControllerUpdate:
         assert updated_bot[0].name == "Updated Bot"
         assert updated_bot[0].system_prompt == "Updated prompt"
         assert updated_bot[0].slug == "updated-bot"  # Check slug
+
+    def test_update_chatbot_slug_exist(self, setup_controller):
+        controller = setup_controller
+
+        
+        create_request = BotCreate(
+            name="Test Bot 1",
+            system_prompt="Test prompt",
+            model="OpenAI",
+            adapter="Slack",
+            slug="existing-slug"  
+        )
+        create_request2 = BotCreate(
+            name="Test Bot 2",
+            system_prompt="Test prompt",
+            model="OpenAI",
+            adapter="Slack",
+            slug="existing-slug-1"  
+        )
+        controller.create_chatbot(create_request)  
+        controller.create_chatbot(create_request2)  
+
+        created_bot = setup_controller.fetch_chatbots(0, 10)
+
+        
+        bot_update_request = BotUpdate(
+            name="Updated Bot",
+            system_prompt="Updated prompt",
+            model="OpenAI",
+            adapter="Slack",
+            slug="existing-slug-1" 
+        )
+
+        with pytest.raises(HTTPException) as exc:
+            controller.update_chatbot(created_bot[0].id, bot_update_request)
+
+        assert exc.value.status_code == 400
+        assert exc.value.detail == "Slug is exist"
         
     def test_update_chatbot_name_required(self, setup_controller):
         controller = setup_controller
@@ -291,3 +353,24 @@ class TestBotControllerUpdate:
         
         assert exc.value.status_code == 400
         assert exc.value.detail == "Slug is required"
+
+class TestBotControllerCheckSlugExist:
+    def test_check_slug_exist_success(self, setup_controller):
+        slug = "existing-slug"
+        
+        setup_controller.service.is_slug_exist = lambda x: True  
+
+        response = setup_controller.check_slug_exist(slug)
+
+        assert response == {"detail": True}  
+
+    def test_check_slug_exist_not_found(self, setup_controller):
+        slug = "non-existent-slug"
+        
+        setup_controller.service.is_slug_exist = lambda x: False  
+
+        response = setup_controller.check_slug_exist(slug)
+
+        assert response == {"detail": False}  
+
+    
