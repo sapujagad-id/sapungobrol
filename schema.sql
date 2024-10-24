13f865de7b22e5ce42bb6cf8066f7596eef6a180
Veivel <25278964+Veivel@users.noreply.github.com>
2024-10-24 23:46:50 +0700

    feat: sql migration for bot fk to user

--- schema.sql
+++ schema.sql
@@ -2,17 +2,6 @@ CREATE TYPE model_engines AS ENUM ('OPENAI', 'ANTHROPIC');
 
 CREATE TYPE message_adapters AS ENUM('SLACK');
 
-CREATE TABLE bots (
-    id UUID PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    system_prompt TEXT NOT NULL,
-    model model_engines NOT NULL,
-    adapter message_adapters NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW(),
-    slug VARCHAR(255)
-);
-
 CREATE TYPE login_methods AS ENUM('GOOGLE');
 
 CREATE TABLE users (
@@ -24,4 +13,17 @@ CREATE TABLE users (
     email_verified BOOLEAN NOT NULL,
     login_method login_methods NOT NULL DEFAULT 'GOOGLE',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE bots (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    user_id UUID, -- FK
+    system_prompt TEXT NOT NULL,
+    model model_engines NOT NULL,
+    adapter message_adapters NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+    slug VARCHAR(255),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
 );
