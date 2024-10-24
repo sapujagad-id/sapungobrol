164f8ff58d4ea11b67e58882e454e269dbac38bf
jasonirvine76 <jason.irvine@ui.ac.id>
2024-10-25 01:54:30 +0700

    [GREEN] feat: add login decorator logic and login view

--- auth/middleware.py
+++ auth/middleware.py
@@ -10,6 +10,31 @@ def login_required(func):
     async def wrapper(*args, **kwargs):
         if kwargs.pop('testing', None):
             return func(*args, **kwargs)
+        
+        config = AppConfig()
+        logger_service = logger.bind(service="AuthMiddleware")
+        request = kwargs.get("request")
+
+        token = request.cookies.get("token")
+        if not token:
+            return RedirectResponse(url="/login", status_code=302)
+
+        try:
+            jwt_secret_key = kwargs.pop('jwt_secret_key', None)
+            if jwt_secret_key is None:
+                jwt_secret_key = config.jwt_secret_key
+            decoded = jwt.decode(
+                token=token, 
+                key=jwt_secret_key,
+                algorithms=["HS256"],
+            )
+            if not decoded:
+                raise HTTPException(status_code=401, detail="Invalid token")
+        except Exception as e:
+            logger_service.info(str(e))
+            return RedirectResponse(url="/login", status_code=302)
+
+        request.state.user_profile = decoded
         return func(*args, **kwargs)
     
     return wrapper
