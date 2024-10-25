ba78aef772865beec70bee6f713dd2b2cef293fc
Arkan Alexei Andrei <arkan.alexei@ui.ac.id>
2024-10-25 13:38:24 +0700

    fix: change return

--- chat/openai_chat.py
+++ chat/openai_chat.py
@@ -17,4 +17,4 @@ class ChatOpenAI(ChatEngine):
             model="gpt-4o-mini",
             messages=self.history,
         )
-        return response["choices"][0]["message"]["content"]
+        return response.choices[0].message.content
