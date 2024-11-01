44c5a869783be20604a749ea519e0b3ff72663fd
Veivel <25278964+Veivel@users.noreply.github.com>
2024-11-01 21:29:00 +0700

    [GREEN] feat: add pydantic model definition, neg tests

--- data_source/test_data_source.py
+++ data_source/test_data_source.py
@@ -22,7 +22,7 @@ class TestDataSource:
             id=uuid4(),
             type=DataSourceType.CSV,
             object_url=None,
-            db_conn_url="sqlite:///example.db",
+            db_conn_url="mysql://user:password@localhost:3210/dbname",
             table_names=["users"],
             created_by=uuid4(),
             created_at=datetime.now(),
@@ -42,3 +42,61 @@ class TestDataSource:
             updated_at=datetime.now()
         )
         data_source.validate()
+
+    def test_missing_table_names_for_sql_type(self):
+        data_source = DataSource(
+            id=uuid4(),
+            type=DataSourceType.SQL,
+            object_url=None,
+            db_conn_url="postgresql://user:password@localhost/dbname",
+            table_names=None,
+            created_by=uuid4(),
+            created_at=datetime.now(),
+            updated_at=datetime.now()
+        )
+        with pytest.raises(TableNameError):
+            data_source.validate()
+
+    def test_missing_table_names_for_csv_type(self):
+        data_source = DataSource(
+            id=uuid4(),
+            type=DataSourceType.CSV,
+            object_url=None,
+            db_conn_url="sqlite:///example.db",
+            table_names=None,
+            created_by=uuid4(),
+            created_at=datetime.now(),
+            updated_at=datetime.now()
+        )
+        with pytest.raises(TableNameError):
+            data_source.validate()
+
+    def test_missing_object_url_for_pdf_type(self):
+        data_source = DataSource(
+            id=uuid4(),
+            type=DataSourceType.PDF,
+            object_url="asd",
+            db_conn_url=None,
+            table_names=None,
+            created_by=uuid4(),
+            created_at=datetime.now(),
+            updated_at=datetime.now()
+        )
+        data_source.object_url = None
+        with pytest.raises(ObjectUrlError):
+            data_source.validate()
+
+    def test_invalid_data_source_type(self):
+        data_source = DataSource(
+            id=uuid4(),
+            type=DataSourceType.PDF,
+            object_url=None,
+            db_conn_url=None,
+            table_names=None,
+            created_by=uuid4(),
+            created_at=datetime.now(),
+            updated_at=datetime.now()
+        )
+        data_source.type = "invalido"
+        with pytest.raises(DataSourceTypeError):
+            data_source.validate()
