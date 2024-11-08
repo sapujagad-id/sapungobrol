6e359b47c26f1baf9de15b40c6186b8a9e6ef3f7
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2024-11-08 23:14:52 +0700

    [GREEN] feat: add reaction event create object

---
+++ adapter/reaction_event.py
@@ -0,0 +1,44 @@
+import enum
+
+from datetime import datetime
+from typing import Any
+from pydantic import BaseModel, ConfigDict, UUID4
+from bot.bot import MessageAdapter
+
+
+class UnknownReaction(Exception):
+    message = "Unknown reaction found"
+
+
+class Reaction(enum.StrEnum):
+    POSITIVE = "Positive"
+    NEGATIVE = "Negative"
+
+
+class ReactionEventCreate(BaseModel):
+    bot_id: UUID4
+    reaction: Reaction
+    source_adapter: MessageAdapter
+    source_adapter_message_id: str
+    source_adapter_user_id: str
+    message: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+    @staticmethod
+    def from_slack_reaction(bot_id: str, message: str, event: dict[str, Any]):
+        if event["reaction"] == "-1":
+            reaction = Reaction.NEGATIVE
+        elif event["reaction"] == "+1":
+            reaction = Reaction.POSITIVE
+        else:
+            raise UnknownReaction
+
+        return ReactionEventCreate(
+            bot_id=bot_id,
+            reaction=reaction,
+            source_adapter=MessageAdapter.SLACK,
+            source_adapter_message_id=event["item"]["ts"],
+            source_adapter_user_id=event["user"],
+            message=message,
+        )
