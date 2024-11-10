f5f8a514d7fed7334b66bbf122e39c8868ec4e07
Veivel <25278964+Veivel@users.noreply.github.com>
2024-11-10 17:51:24 +0700

    [REFACTOR] object-oriented unit tests

--- document/test_repository.py
+++ document/test_repository.py
@@ -20,95 +20,97 @@ def get_sample_document_models():
             object_name="pdf_object"
         )
     ]
+    
+class TestDocumentRepository:
 
-def test_create_document(setup_repository, setup_session):
-    doc_create = DocumentCreate(
-        type=DocumentType.CSV.value,
-        title="Sample Document",
-        object_name="sample_object",
-        created_at=datetime.now(),
-        updated_at=datetime.now()
-    )
-    
-    setup_repository.create_document(doc_create)
-    
-    # verify document exists
-    with setup_session() as session:
-        result = session.query(DocumentModel).filter_by(object_name="sample_object").first()
+    def test_create_document(self, setup_repository, setup_session):
+        doc_create = DocumentCreate(
+            type=DocumentType.CSV.value,
+            title="Sample Document",
+            object_name="sample_object",
+            created_at=datetime.now(),
+            updated_at=datetime.now()
+        )
+        
+        setup_repository.create_document(doc_create)
+        
+        # verify document exists
+        with setup_session() as session:
+            result = session.query(DocumentModel).filter_by(object_name="sample_object").first()
+            assert result is not None
+            assert result.title == "Sample Document"
+            assert result.type == DocumentType.CSV.value
+
+
+    def test_get_document_by_id(self, setup_repository, setup_session):
+        doc_id = uuid4()
+        document = DocumentModel(
+            id=doc_id,
+            type=DocumentType.PDF.value,
+            title="PDF Document",
+            object_name="pdf_object",
+            created_at=datetime.now(),
+            updated_at=datetime.now()
+        )
+        with setup_session() as session:
+            session.add(document)
+            session.commit()
+
+        result = setup_repository.get_document_by_id(doc_id)
         assert result is not None
-        assert result.title == "Sample Document"
-        assert result.type == DocumentType.CSV.value
+        assert result.title == "PDF Document"
+        assert result.object_name == "pdf_object"
 
 
-def test_get_document_by_id(setup_repository, setup_session):
-    doc_id = uuid4()
-    document = DocumentModel(
-        id=doc_id,
-        type=DocumentType.PDF.value,
-        title="PDF Document",
-        object_name="pdf_object",
-        created_at=datetime.now(),
-        updated_at=datetime.now()
-    )
-    with setup_session() as session:
-        session.add(document)
-        session.commit()
+    def test_get_document_by_name(self, setup_repository, setup_session):
+        document = DocumentModel(
+            id=uuid4(),
+            type=DocumentType.TXT.value,
+            title="Text Document",
+            object_name="text_object",
+            created_at=datetime.now(),
+            updated_at=datetime.now()
+        )
+        with setup_session() as session:
+            session.add(document)
+            session.commit()
 
-    result = setup_repository.get_document_by_id(doc_id)
-    assert result is not None
-    assert result.title == "PDF Document"
-    assert result.object_name == "pdf_object"
-
-
-def test_get_document_by_name(setup_repository, setup_session):
-    document = DocumentModel(
-        id=uuid4(),
-        type=DocumentType.TXT.value,
-        title="Text Document",
-        object_name="text_object",
-        created_at=datetime.now(),
-        updated_at=datetime.now()
-    )
-    with setup_session() as session:
-        session.add(document)
-        session.commit()
-
-    result = setup_repository.get_document_by_name("text_object")
-    assert result is not None
-    assert result.title == "Text Document"
-    assert result.type == DocumentType.TXT.value
+        result = setup_repository.get_document_by_name("text_object")
+        assert result is not None
+        assert result.title == "Text Document"
+        assert result.type == DocumentType.TXT.value
 
 
-def test_get_documents_with_filters(setup_repository, setup_session):
-    documents = get_sample_document_models()
-    with setup_session() as session:
-        session.bulk_save_objects(documents)
-        session.commit()
+    def test_get_documents_with_filters(self, setup_repository, setup_session):
+        documents = get_sample_document_models()
+        with setup_session() as session:
+            session.bulk_save_objects(documents)
+            session.commit()
 
-    # filter to fetch only the PDF document
-    doc_filter = DocumentFilter(object_name="pdf_object", title="PDF Document", created_before=datetime.now())
-    result = setup_repository.get_documents(doc_filter)
-    assert len(result) == 1
-    assert result[0].title == "PDF Document"
-    assert result[0].object_name == "pdf_object"
-    
-def test_get_documents_without_filters(setup_repository, setup_session):
-    documents = get_sample_document_models()
-    with setup_session() as session:
-        session.bulk_save_objects(documents)
-        session.commit()
+        # filter to fetch only the PDF document
+        doc_filter = DocumentFilter(object_name="pdf_object", title="PDF Document", created_before=datetime.now())
+        result = setup_repository.get_documents(doc_filter)
+        assert len(result) == 1
+        assert result[0].title == "PDF Document"
+        assert result[0].object_name == "pdf_object"
+        
+    def test_get_documents_without_filters(self, setup_repository, setup_session):
+        documents = get_sample_document_models()
+        with setup_session() as session:
+            session.bulk_save_objects(documents)
+            session.commit()
 
-    result = setup_repository.get_documents(None)
-    assert len(result) == 2
+        result = setup_repository.get_documents(None)
+        assert len(result) == 2
 
 
-def test_get_documents_without_filters(setup_repository, setup_session):
-    documents = get_sample_document_models()
-    with setup_session() as session:
-        session.bulk_save_objects(documents)
-        session.commit()
+    def test_get_documents_without_filters(self, setup_repository, setup_session):
+        documents = get_sample_document_models()
+        with setup_session() as session:
+            session.bulk_save_objects(documents)
+            session.commit()
 
-    # retrieve all documents without filters
-    doc_filter = DocumentFilter()
-    result = setup_repository.get_documents(doc_filter)
-    assert len(result) == 2
+        # retrieve all documents without filters
+        doc_filter = DocumentFilter()
+        result = setup_repository.get_documents(doc_filter)
+        assert len(result) == 2
