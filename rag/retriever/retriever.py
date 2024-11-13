d6eb194a1b8a26ea80cd1e378649cdf032511737
nyoosteven <stevenchrist123@gmail.com>
2024-11-13 13:37:04 +0700

    [GREEN] feat: add retriever function

---
+++ rag/retriever/retriever.py
@@ -0,0 +1,22 @@
+class Retriever:
+    def __init__(self):
+        pass
+
+    def _retrieve_context_vector(self, query, access_level, top_k=5) -> list:
+        return []
+
+    def _retrieve_context_tabular(self, query, access_level) -> str:
+        return ''
+
+    def query(self, query, access_level, top_k=5):
+        # Retrieve Nodes based on the vector
+        context_vector = self._retrieve_context_vector(query, access_level, top_k)
+
+        # Retrieve tabular context
+        context_tabular = self._retrieve_context_tabular(query, access_level)
+
+        # Create the final context
+        final_context = context_vector.copy()
+        final_context.append(context_tabular)
+
+        return final_context
