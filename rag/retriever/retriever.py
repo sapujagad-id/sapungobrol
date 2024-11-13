051daf315afe2351061840fef3fe5b29fdb99a39
Insta-x <uttsadajason@outlook.com>
2024-11-13 17:47:01 +0700

    [GREEN] feat: retrieve vector context

--- rag/retriever/retriever.py
+++ rag/retriever/retriever.py
@@ -6,7 +6,13 @@ class Retriever:
         self.postgres_handler = postgres_handler
 
     def _retrieve_context_vector(self, query, access_level, top_k=5) -> list:
-        return []
+        embedding_result = openai.embeddings.create(
+            input=query,
+            model="text-embedding-3-small"
+        )
+        query_vector = embedding_result.data[0].embedding
+        
+        return self.postgres_handler.query(query_vector, access_level=access_level, top_k=top_k)
 
     def _retrieve_context_tabular(self, query, access_level) -> str:
         return ''
