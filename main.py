ae2c6c05aea5f8597e3466f610c253457d31b54c
Veivel <25278964+Veivel@users.noreply.github.com>
2024-11-16 00:04:21 +0700

    feat: add document creation form and logic

--- main.py
+++ main.py
@@ -157,8 +157,8 @@ if __name__ == "__main__":
     )
     
     app.add_api_route(
-        "/new-data-source",
-        endpoint=data_source_view.new_document_view,
+        "/create-document",
+        endpoint=document_view.new_document_view,
         response_class=HTMLResponse,
         description="Page that displays Document Creation form",
     )
