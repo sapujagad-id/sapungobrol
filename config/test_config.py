6d35d561eb78f59a9577ada2af52f6d67311a6f4
Abdul Rafi <63101627+TakeMeGH@users.noreply.github.com>
2024-11-15 08:12:31 +0700

    [Refactor] Add necesery config test
    Update the config test for default port testing

--- config/test_config.py
+++ config/test_config.py
@@ -34,6 +34,10 @@ class TestAppConfig:
         monkeypatch.setenv("AWS_REGION", "ap-test")
         monkeypatch.setenv("AWS_ACCESS_KEY_ID", "test")
         monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", "test")
+        monkeypatch.setenv("SLACK_CLIENT_ID", "123456.789")
+        monkeypatch.setenv("SLACK_CLIENT_SECRET", "1a2b3c4d5e6f")
+        monkeypatch.setenv("SLACK_SCOPES", "channels:history")
+
 
         config = AppConfig()
 
