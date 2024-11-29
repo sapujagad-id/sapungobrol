380e70872929d5068e64ea36c27a9492bdc387a1
jasonirvine76 <jason.irvine@ui.ac.id>
2024-11-29 16:29:58 +0700

    [GREEN] feat: add fetch reaction in reaction event repository

--- adapter/reaction_event_repository.py
+++ adapter/reaction_event_repository.py
@@ -1,7 +1,7 @@
 from abc import abstractmethod, ABC
 from datetime import datetime
 from loguru import logger
-from sqlalchemy import Column, Uuid, Enum, String, Text, DateTime
+from sqlalchemy import Column, Uuid, Enum, String, Text, DateTime, func
 from sqlalchemy.orm import sessionmaker, Session, declarative_base
 from uuid import uuid4
 
@@ -29,6 +29,14 @@ class ReactionEventRepository(ABC):
     def create_reaction_event(self, event_create: ReactionEventCreate):
         pass
 
+    @abstractmethod
+    def fetch_reaction_event_by_bot_id(self, bot_id: str, reaction: Reaction):
+        pass
+
+    @abstractmethod
+    def fetch_total_conversation(self, bot_id:str):
+        pass
+
 
 class PostgresReactionEventRepository(ReactionEventRepository):
     def __init__(self, session: sessionmaker[Session]) -> None:
@@ -51,3 +59,36 @@ class PostgresReactionEventRepository(ReactionEventRepository):
                 )
                 session.add(new_reaction_event)
                 session.commit()
+
+    def fetch_reaction_event_by_bot_id(self, bot_id: str, reaction: Reaction) -> list[ReactionEventModel]:
+        with self.create_session() as session:
+            result = (
+                session.query(
+                    ReactionEventModel.source_adapter_message_id,
+                    func.count(ReactionEventModel.id).label("reactions_count"),
+                )
+                .filter(
+                    ReactionEventModel.bot_id == bot_id,
+                    ReactionEventModel.reaction == reaction,
+                )
+                .group_by(ReactionEventModel.source_adapter_message_id)
+                .all()
+            )
+            return [
+                {
+                    "source_adapter_message_id": row.source_adapter_message_id,
+                    "reactions_count": row.reactions_count,
+                }
+                for row in result
+            ]
+
+    def fetch_total_conversation(self, bot_id:str):
+        with self.create_session() as session:
+            with self.logger.catch(message="fetching reaction events error", reraise=True):
+                total_conversations = (
+                    session.query(ReactionEventModel.source_adapter_message_id)
+                    .filter(ReactionEventModel.bot_id == bot_id)
+                    .distinct()
+                    .count()
+                )
+                return total_conversations
