da6c8183a9fff8687dfa6b4ec09f9f8b51e484a4
jasonirvine76 <jason.irvine@ui.ac.id>
2024-12-06 20:06:37 +0700

    [RED] test: add new tests for service

--- document/test_service.py
+++ document/test_service.py
@@ -1,13 +1,15 @@
+from datetime import datetime
+from uuid import uuid4
+import pytest
+from sqlalchemy import create_engine
+from document.document import DocumentType, FileExtensionDoesNotMatchType, FileSizeExceededMaxLimit, ObjectNameError
+from document.dto import DocumentCreate, DocumentFilter
+from document.repository import DocumentModel, PostgresDocumentRepository
+from document.service import DocumentServiceV1
 import io
 from unittest.mock import ANY, MagicMock
-
 import pytest
-from botocore.exceptions import (BotoCoreError,  # type: ignore
-                                 NoCredentialsError, PartialCredentialsError)
-
-from document.document import DocumentType, ObjectNameError
-from document.dto import DocumentCreate, DocumentFilter
-
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError, BotoCoreError # type: ignore
 
 class TestDocumentService:
     def test_get_documents(self, setup_service, setup_documents):
@@ -33,7 +35,11 @@ class TestDocumentService:
             object_name="new_doc.txt",
             access_level=0,
         )
-        setup_service.create_document(new_document)
+        mock_file_content = MagicMock()
+        mock_file_content.file = io.BytesIO(b"test data")
+
+        mock_file_content.filename = "test_object_name.txt"
+        setup_service.create_document(new_document, mock_file_content)
         document = setup_repository.get_documents(DocumentFilter(object_name="new_doc.txt"))
         assert len(document) == 1
         assert document[0].title == "New Document"
@@ -45,10 +51,14 @@ class TestDocumentService:
             object_name="doc1.csv",
             access_level=0,
         )
-        setup_service.create_document(existing_document)
+        mock_file_content = MagicMock()
+        mock_file_content.file = io.BytesIO(b"test data")
+
+        mock_file_content.filename = "doc1.csv"
+        setup_service.create_document(existing_document, mock_file_content)
         existing_document.title = "Duplicate Document"
         with pytest.raises(ObjectNameError):
-            setup_service.create_document(existing_document)
+            setup_service.create_document(existing_document, mock_file_content)
 
 
     def test_upload_document_success(self, setup_service):
@@ -56,43 +66,66 @@ class TestDocumentService:
         mock_file_content = MagicMock()
         mock_file_content.file = io.BytesIO(b"test data")
 
+        mock_file_content.filename = "test_object_name.txt"
+
         # Call the upload_document method
-        setup_service.upload_document(mock_file_content, "test_object_name")
+        setup_service.upload_document(mock_file_content, "test_object_name.txt", "txt")
 
         # Assert that the S3 upload_fileobj method was called once
         setup_service.mock_s3_client.upload_fileobj.assert_called_once_with(
-            ANY, "test_bucket", "test_object_name"
+            ANY, "test_bucket", "test_object_name.txt"
         )
+
+    def test_upload_document_file_extension_does_not_match_type(self, setup_service):
+        mock_file_content = MagicMock()
+        mock_file_content.file = io.BytesIO(b"test data")
+
+        mock_file_content.name = "test_object_name.txt"
+
+        with pytest.raises(FileExtensionDoesNotMatchType):
+            setup_service.upload_document(mock_file_content, "test_object_name.txt", "csv")
+        
+    
+    def test_upload_document_max_file_size(self, setup_service):
+        mock_file_content = MagicMock()
+        mock_file_content.file = io.BytesIO(b"a" * (11 * 1024 * 1024))  
+
+        mock_file_content.name = "test_object_name.txt"  
+
+        with pytest.raises(FileSizeExceededMaxLimit):
+            setup_service.upload_document(mock_file_content, "test_object_name.txt", "txt")
 
 
     def test_upload_document_no_credentials_error(self, setup_service):
-        # Simulate NoCredentialsError
         setup_service.mock_s3_client.upload_fileobj.side_effect = NoCredentialsError
 
         mock_file_content = MagicMock()
         mock_file_content.file.read.return_value = b"test data"
+        mock_file_content.filename = "test_object_name.txt"
 
         with pytest.raises(NoCredentialsError):
-            setup_service.upload_document(mock_file_content, "test_object_name")
+            setup_service.upload_document(mock_file_content, "test_object_name.txt", "txt")
 
     def test_upload_document_partial_credentials_error(self, setup_service):
-        # Simulate PartialCredentialsError
         setup_service.mock_s3_client.upload_fileobj.side_effect = PartialCredentialsError(
         provider='aws', cred_var='aws_secret_access_key'
         )
 
         mock_file_content = MagicMock()
         mock_file_content.file = io.BytesIO(b"test data")
+        mock_file_content.filename = "test_object_name.txt"
+
 
         with pytest.raises(PartialCredentialsError):
-            setup_service.upload_document(mock_file_content, "test_object_name")
+            setup_service.upload_document(mock_file_content, "test_object_name.txt", "txt")
 
     def test_upload_document_botocore_error(self, setup_service):
-        # Simulate a generic BotoCoreError
         setup_service.mock_s3_client.upload_fileobj.side_effect = BotoCoreError
 
         mock_file_content = MagicMock()
         mock_file_content.file.read.return_value = b"test data"
+        mock_file_content.filename = "test_object_name.txt"
+
 
         with pytest.raises(BotoCoreError):
-            setup_service.upload_document(mock_file_content, "test_object_name")
+            setup_service.upload_document(mock_file_content, "test_object_name.txt", "txt")
