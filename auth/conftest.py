20cfc9235225f1e450c0e639b59d3570c7df7362
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2024-12-09 10:49:11 +0700

    feat: implement edit user access logic

--- auth/conftest.py
+++ auth/conftest.py
@@ -15,61 +15,67 @@ from auth.service import AuthServiceV1
 
 TEST_DATABASE_URL = "sqlite:///:memory:"  # Change as needed for your test setu
 
+
 @pytest.fixture()
 def session(setup_database):
     """Create a new database session for each test."""
-    session_local = sessionmaker(
-        bind=setup_database, 
-        expire_on_commit=False
-    )
+    session_local = sessionmaker(bind=setup_database, expire_on_commit=False)
     session = session_local()
     yield session
     session.close()
+
 
 @pytest.fixture()
 def setup_repository(session):
     """Set up the repository with the test session."""
     repository = PostgresAuthRepository(session=lambda: session)
     return repository
-  
+
+
 @pytest.fixture()
 def setup_google_credentials():
-  return GoogleCredentials(
-    client_id="xxx",
-    client_secret="xxx",
-    redirect_uri="http://localhost:8000/api/auth/callback/google",
-  )
+    return GoogleCredentials(
+        client_id="xxx",
+        client_secret="xxx",
+        redirect_uri="http://localhost:8000/api/auth/callback/google",
+    )
+
 
 @pytest.fixture()
 def setup_service():
     """Set up a mocked service."""
     service = Mock(spec=AuthServiceV1)
     service.repository = Mock(spec=PostgresAuthRepository)
-    
+
     return service
+
 
 @pytest.fixture()
 def setup_jwt_secret():
     """Set up JWT secret for AuthService"""
     return "some_arbitrary_string_here"
 
+
 @pytest.fixture()
 def setup_real_service(setup_repository, setup_google_credentials, setup_jwt_secret):
     """Set up the REAL service with the repository."""
     service = AuthServiceV1(
-      setup_repository,
-      setup_google_credentials,
-      "http://localhost:8000",
-      setup_jwt_secret,
-      "example@broom.id"
-      )
+        setup_repository,
+        setup_google_credentials,
+        "http://localhost:8000",
+        setup_jwt_secret,
+        "example@broom.id",
+        5,
+    )
     return service
+
 
 @pytest.fixture()
 def setup_controller(setup_service):
     """Set up the controller with the service."""
     controller = AuthControllerV1(setup_service)
     return controller
+
 
 @pytest.fixture()
 def setup_database():
@@ -79,16 +85,17 @@ def setup_database():
     yield engine
     UserModel.metadata.drop_all(engine)
 
+
 @pytest.fixture
 def valid_token(setup_jwt_secret):
     # Generate a valid JWT token for testing
     return jwt.encode({"sub": "user_id"}, key=setup_jwt_secret, algorithm="HS256")
 
+
 @pytest.fixture
 def invalid_token(setup_jwt_secret):
     # Generate an invalid JWT token (e.g., altered or expired)
     return "invalid.token.string"
-
 
 
 @pytest.fixture
@@ -124,4 +131,3 @@ def app(setup_jwt_secret):
 @pytest.fixture
 def client(app):
     return TestClient(app)
-
--- auth/repository.py
+++ auth/repository.py
@@ -1,14 +1,15 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
-from uuid import uuid4
+from uuid import uuid4, UUID
 
 from loguru import logger
-from sqlalchemy import Boolean, Column, DateTime, Enum, Integer, String, Uuid
+from sqlalchemy import Boolean, Column, DateTime, Enum, Integer, String, Uuid, update
 from sqlalchemy.orm import Session, declarative_base, load_only, sessionmaker
 
 from auth.user import GoogleUserInfo, LoginMethod
 
 Base = declarative_base()
+
 
 class UserModel(Base):
     """
@@ -26,70 +27,101 @@ class UserModel(Base):
     email_verified = Column(Boolean, nullable=False)
     login_method = Column(Enum(LoginMethod, validate_strings=True), nullable=False)
     created_at = Column(DateTime, default=datetime.now)
-    access_level = Column(Integer, default=0, nullable=False)  
+    access_level = Column(Integer, default=0, nullable=False)
 
     def __repr__(self):
         return f"User id:{self.id}"
-    
+
+
 class AuthRepository(ABC):
     @abstractmethod
     def find_user_by_sub(self, sub: str) -> UserModel | None:
         pass
-    
+
     @abstractmethod
     def find_user_by_email(self, email: str) -> UserModel | None:
         pass
-    
+
     @abstractmethod
     def add_google_user(self, user_info_json: GoogleUserInfo) -> None:
         pass
 
     @abstractmethod
+    def update_user_access(self, user_id: UUID, access_level: int):
+        pass
+
+    @abstractmethod
     def get_all_users_basic_info(self) -> list[dict]:
         pass
-      
+
+
 class PostgresAuthRepository(AuthRepository):
     def __init__(self, session: sessionmaker) -> None:
         self.create_session = session
         self.logger = logger.bind(service="PostgresAuthRepository")
-        
+
     def find_user_by_sub(self, sub: str) -> UserModel | None:
         with self.create_session() as session:
             with self.logger.catch(
-                message=f"User with sub: {sub} not found", 
+                message=f"User with sub: {sub} not found",
                 reraise=True,
             ):
                 return session.query(UserModel).filter(UserModel.sub == sub).first()
-            
+
     def find_user_by_email(self, email: str) -> UserModel | None:
         with self.create_session() as session:
             with self.logger.catch(
-                message=f"User with email: {email} not found", 
+                message=f"User with email: {email} not found",
                 reraise=True,
             ):
-                assert isinstance(session, Session) 
+                assert isinstance(session, Session)
                 return session.query(UserModel).filter(UserModel.email == email).first()
-            
+
     def add_google_user(self, user_info_json: GoogleUserInfo) -> None:
         with self.create_session() as session:
             with self.logger.catch(message="create user error", reraise=True):
                 assert isinstance(session, Session)
                 user = UserModel(
-                    id = uuid4(),
-                    sub = user_info_json['sub'],
-                    name = user_info_json['name'],
-                    picture = user_info_json['picture'],
-                    email = user_info_json['email'],
-                    email_verified = user_info_json['email_verified'],
-                    login_method = LoginMethod.GOOGLE,
+                    id=uuid4(),
+                    sub=user_info_json["sub"],
+                    name=user_info_json["name"],
+                    picture=user_info_json["picture"],
+                    email=user_info_json["email"],
+                    email_verified=user_info_json["email_verified"],
+                    login_method=LoginMethod.GOOGLE,
                 )
                 session.add(user)
                 session.commit()
-    
+
+    # This will be tested later
+    def update_user_access(self, user_id: UUID, access_level: int):  # pragma: no cover
+        with self.create_session() as session:
+            with self.logger.catch(message="update user access error", reraise=True):
+                assert isinstance(session, Session)
+
+                stmt = (
+                    update(UserModel)
+                    .where(UserModel.id == user_id)
+                    .values(access_level=access_level)
+                )
+
+                session.execute(stmt)
+
+                session.commit()
+
     def get_all_users_basic_info(self) -> list[dict]:
         with self.create_session() as session:
             with self.logger.catch(message="get all users error", reraise=True):
                 assert isinstance(session, Session)
-                load_fields = [getattr(UserModel, field) for field in UserModel.USER_BASIC_INFO_FIELDS]
+                load_fields = [
+                    getattr(UserModel, field)
+                    for field in UserModel.USER_BASIC_INFO_FIELDS
+                ]
                 users = session.query(UserModel).options(load_only(*load_fields)).all()
-                return [{field: getattr(user, field) for field in UserModel.USER_BASIC_INFO_FIELDS} for user in users]
+                return [
+                    {
+                        field: getattr(user, field)
+                        for field in UserModel.USER_BASIC_INFO_FIELDS
+                    }
+                    for user in users
+                ]
