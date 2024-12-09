6a743fcc72e5aa9e5537d678e461c79ad1ce5edb
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2024-12-09 10:48:55 +0700

    feat: implement edit user access controller

--- auth/controller.py
+++ auth/controller.py
@@ -5,82 +5,108 @@ from fastapi.responses import RedirectResponse
 from jose import ExpiredSignatureError, JWTError
 from loguru import logger
 
-from auth.dto import ProfileResponse
-from auth.exceptions import NoTokenSupplied, UserNotFound, UserUnauthorized
+from auth.dto import ProfileResponse, EditUserAccess
+from auth.exceptions import (
+    NoTokenSupplied,
+    UserNotFound,
+    UserUnauthorized,
+    InvalidAccessLevel,
+)
 from auth.service import AuthService
 
 
 class AuthController(ABC):
-  @abstractmethod
-  async def logout(self):
-    pass
-  
-  @abstractmethod
-  async def login_redirect_google(self) -> RedirectResponse:
-    pass
-  
-  @abstractmethod
-  async def authorize_google(self, request: Request, code: str) -> Response:
-    pass
+    @abstractmethod
+    async def logout(self):
+        pass
 
-  @abstractmethod
-  def get_all_users_basic_info(self, request: Request):
-    pass
-  
-  @abstractmethod
-  async def user_profile_google(self, request: Request) -> ProfileResponse:
-    pass
+    @abstractmethod
+    async def login_redirect_google(self) -> RedirectResponse:
+        pass
+
+    @abstractmethod
+    async def authorize_google(self, request: Request, code: str) -> Response:
+        pass
+
+    @abstractmethod
+    def get_all_users_basic_info(self, request: Request):
+        pass
+
+    @abstractmethod
+    def edit_user_access(self, user_id: str, body: EditUserAccess) -> dict[str, str]:
+        pass
+
+    @abstractmethod
+    async def user_profile_google(self, request: Request) -> ProfileResponse:
+        pass
+
 
 class AuthControllerV1(AuthController):
-  def __init__(self, service: AuthService) -> None:
-    super().__init__() 
-    self.service = service
-    self.logger = logger.bind(service="AuthController")
-    
-  def logout(self) -> Response:
-    response = RedirectResponse("/")
-    try:
-      response.delete_cookie("token")
-      response.delete_cookie("is_admin")
-    except:
-      pass
-    return response
-    
-  def login_redirect_google(self) -> Response:
-    response = self.service.login_redirect_google()
-    return response
-    
-  def authorize_google(self, request: Request, code: str) -> Response :
-    response = self.service.authorize_google(request, code)
+    def __init__(self, service: AuthService) -> None:
+        super().__init__()
+        self.service = service
+        self.logger = logger.bind(service="AuthController")
 
-    return response
-  
-  def get_all_users_basic_info(self, request: Request):
-    token = request.cookies.get("token")
-    try:
-      all_user = self.service.get_all_users_basic_info(token)
-      return all_user
-    except (NoTokenSupplied, UserUnauthorized):
-      raise HTTPException(status_code=401, detail="You are not authenticated")
-    except ExpiredSignatureError:
-      raise HTTPException(status_code=400, detail="Your token has expired, please login again")    
-    except JWTError:
-      raise HTTPException(status_code=400, detail="Invalid token signature")
-    except UserNotFound:
-      raise HTTPException(status_code=404, detail="User not found")
+    def logout(self) -> Response:
+        response = RedirectResponse("/")
+        try:
+            response.delete_cookie("token")
+            response.delete_cookie("is_admin")
+        except:
+            pass
+        return response
 
-  def user_profile_google(self, request: Request) -> ProfileResponse:
-    token = request.cookies.get("token")
-    try:
-      user = self.service.get_user_profile(token)
-      return ProfileResponse(
-        data=user,
-      )
-    except NoTokenSupplied:
-      raise HTTPException(status_code=401, detail="You are not authenticated")
-    except ExpiredSignatureError:
-      raise HTTPException(status_code=400, detail="Your token has expired, please login again")    
-    except JWTError:
-      raise HTTPException(status_code=400, detail="Invalid token signature")
-    except UserNotFound:
-      raise HTTPException(status_code=404, detail="User not found")
+    def login_redirect_google(self) -> Response:
+        response = self.service.login_redirect_google()
+        return response
+
+    def authorize_google(self, request: Request, code: str) -> Response:
+        response = self.service.authorize_google(request, code)
+
+        return response
+
+    def get_all_users_basic_info(self, request: Request):
+        token = request.cookies.get("token")
+        try:
+            all_user = self.service.get_all_users_basic_info(token)
+            return all_user
+        except (NoTokenSupplied, UserUnauthorized):
+            raise HTTPException(status_code=401, detail="You are not authenticated")
+        except ExpiredSignatureError:
+            raise HTTPException(
+                status_code=400, detail="Your token has expired, please login again"
+            )
+        except JWTError:
+            raise HTTPException(status_code=400, detail="Invalid token signature")
+        except UserNotFound:
+            raise HTTPException(status_code=404, detail="User not found")
+
+    # This will be tested later
+    def edit_user_access(
+        self, user_id: str, body: EditUserAccess
+    ) -> dict[str, str]:  # pragma: no cover
+        try:
+            self.service.edit_user_access(user_id, body)
+            return {"message": "Access level updated successfully"}
+        except InvalidAccessLevel:
+            raise HTTPException(
+                status_code=400, detail=InvalidAccessLevel.default_message
+            )
+
+    def user_profile_google(self, request: Request) -> ProfileResponse:
+        token = request.cookies.get("token")
+        try:
+            user = self.service.get_user_profile(token)
+            return ProfileResponse(
+                data=user,
+            )
+        except NoTokenSupplied:
+            raise HTTPException(status_code=401, detail="You are not authenticated")
+        except ExpiredSignatureError:
+            raise HTTPException(
+                status_code=400, detail="Your token has expired, please login again"
+            )
+        except JWTError:
+            raise HTTPException(status_code=400, detail="Invalid token signature")
+        except UserNotFound:
+            raise HTTPException(status_code=404, detail="User not found")
--- auth/exceptions.py
+++ auth/exceptions.py
@@ -3,32 +3,49 @@ class NoTokenSupplied(Exception):
         self.message = message
         super().__init__(self.message)
 
+
 class UserUnauthorized(Exception):
     def __init__(self, message="Unauthorized"):
         self.message = message
         super().__init__(self.message)
+
 
 class UserNotFound(Exception):
     def __init__(self, message="This user does not exist"):
         self.message = message
         super().__init__(self.message)
 
+
 class SubNotFound(Exception):
     def __init__(self, message="Subject identifier cannot be empty"):
         self.message = message
         super().__init__(self.message)
+
 
 class InvalidEmail(Exception):
     def __init__(self, message="Email cannot be empty"):
         self.message = message
         super().__init__(self.message)
 
+
 class InvalidName(Exception):
     def __init__(self, message="Name cannot be empty"):
         self.message = message
         super().__init__(self.message)
 
+
 class InvalidPictureURL(Exception):
     def __init__(self, message="Picture URL must be a valid HTTP/HTTPS URL"):
         self.message = message
         super().__init__(self.message)
+
+
+# This will be tested later
+class InvalidAccessLevel(Exception):  # pragma: no cover
+    default_message = "Invalid access level"
+
+    def __init__(self, *args):
+        if args:
+            super().__init__(*args)
+        else:
+            super().__init__(self.default_message)
